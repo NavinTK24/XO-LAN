8e75d10d5675a788390f76bfbde4eaf6ae40d2e6
NavinTK24 <tk.navin.24@gmail.com>
2025-07-04 17:24:28 +0530

    Win notification added

--- XO-testing-server.js
+++ XO-testing-server.js
@@ -8,18 +8,18 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname,'XO-testing.html'));
 });
 
-let count = 0;
+//let count = 0;
 
 io.on('connection', (socket) => {
     console.log("Player connected: "+socket.id);
 
     socket.on('Xplayer', (data) => {
         count++;
-        io.emit('Xplayer', {id: data.id, count: count});
+        io.emit('Xplayer', {id: data.id, count: count, value: data.value});
     });
     socket.on('Oplayer', (data) => {
         count++;
-        io.emit('Oplayer', {id: data.id, count: count});
+        io.emit('Oplayer', {id: data.id, count: count, value: data.value});
     });
 });
 
