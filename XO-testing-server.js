83e4fb7a195ab2fe3bba7a9ce48157bd8d1c7ed0
NavinTK24 <tk.navin.24@gmail.com>
2025-07-13 22:56:22 +0530

    Works for draw game

--- XO-testing-server.js
+++ XO-testing-server.js
@@ -5,20 +5,34 @@ const io = require('socket.io')(http);
 const path = require('path');
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,'XO-testing.html'));
+    res.sendFile(path.join(__dirname,'testing','index4.html'));
 });
+
+var count = 0;
 
 io.on('connection', (socket) => {
     console.log("Player connected: "+socket.id);
-    var count = 0;
+    console.log('Clients connected:', io.engine.clientsCount);
+
+    //Decides and emits the data to the client
     socket.on('playerChoice', (data) => {
         const player = (count%2 === 0)? 'X' : 'O';
         io.emit('updatedDiv', {
             id: data.id,
             value: Number(data.value),
             player: player,
+            count: count+1,
         });
+        
         count++;
+        
+    });
+
+    //emits reset signal when button is pressed
+    socket.on('resetEverything', () => {
+        console.log(`Reset requested by ${socket.id}`);
+        count = 0;
+        io.emit('resetBoardForAll');
     });
 });
 
