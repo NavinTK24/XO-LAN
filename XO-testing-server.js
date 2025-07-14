00724ce49f26b6ab9e780dac91967ee16ae4ecf5
NavinTK24 <tk.navin.24@gmail.com>
2025-07-14 17:20:24 +0530

    Works for draw game

--- XO-testing-server.js
+++ XO-testing-server.js
@@ -9,22 +9,46 @@ app.get('/', (req, res) => {
 });
 
 var count = 0;
+var Xclient = '';
+var Oclient = '';
 
 io.on('connection', (socket) => {
     console.log("Player connected: "+socket.id);
     console.log('Clients connected:', io.engine.clientsCount);
 
+    if(count === 0) {
+        if(!Xclient) {
+            Xclient = socket.id;
+        } else if (!Oclient) {
+            Oclient = socket.id;
+        }
+    }
+    
     //Decides and emits the data to the client
     socket.on('playerChoice', (data) => {
         const player = (count%2 === 0)? 'X' : 'O';
-        io.emit('updatedDiv', {
-            id: data.id,
-            value: Number(data.value),
-            player: player,
-            count: count+1,
-        });
-        
-        count++;
+
+        if (io.engine.clientsCount === 1 || Oclient === '') {
+            io.emit('updatedDiv', {
+                id: data.id,
+                value: Number(data.value),
+                player: player,
+                count: count+1,
+            }); 
+            count++;
+        }
+        else if((io.engine.clientsCount === 1 || socket.id === Xclient && player === 'X') || (socket.id === Oclient && player === 'O')) {
+            io.emit('updatedDiv', {
+                id: data.id,
+                value: Number(data.value),
+                player: player,
+                count: count+1,
+            }); 
+            count++;
+        } else {
+            console(`Invalid move attempt by ${socket.id}`);
+        }
+
         
     });
 
@@ -33,6 +57,18 @@ io.on('connection', (socket) => {
         console.log(`Reset requested by ${socket.id}`);
         count = 0;
         io.emit('resetBoardForAll');
+    });
+    
+    socket.on('disconnect', () => {
+        console.log("Player disconnected: "+socket.id);
+        // Reset roles if one of the players leaves
+        if(socket.id === Xclient || socket.id === Oclient) {
+            console.log("Resetting game due to disconnect");
+            count = 0;
+            Xclient = '';
+            Oclient = '';
+            io.emit('resetBoardForAll');
+        }
     });
 });
 
